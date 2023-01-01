6b952aa6f57f0eac4d386a35164bbbd9b792a6b7
tranbason241@gmail.com <tranbason241@gmail.com>
2023-01-01 11:15:06 +0700

    update firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,15 +5,25 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyD5do7PbI3lWpwuT5KSmejah5TimQzK-ng",
+//     authDomain: "deliveryfood-9c436.firebaseapp.com",
+//     projectId: "deliveryfood-9c436",
+//     storageBucket: "deliveryfood-9c436.appspot.com",
+//     messagingSenderId: "682756485581",
+//     appId: "1:682756485581:web:7bcb0b616d335b81285484",
+//     measurementId: "G-0HQRH50TQ3",
+// };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyD5do7PbI3lWpwuT5KSmejah5TimQzK-ng",
-    authDomain: "deliveryfood-9c436.firebaseapp.com",
-    projectId: "deliveryfood-9c436",
-    storageBucket: "deliveryfood-9c436.appspot.com",
-    messagingSenderId: "682756485581",
-    appId: "1:682756485581:web:7bcb0b616d335b81285484",
-    measurementId: "G-0HQRH50TQ3",
-};
+    apiKey: "AIzaSyDc5FQ0odIKMJOWG1QWxEGcyoeJAnO1HOE",
+    authDomain: "vinhome-e51b5.firebaseapp.com",
+    projectId: "vinhome-e51b5",
+    storageBucket: "vinhome-e51b5.appspot.com",
+    messagingSenderId: "479702387333",
+    appId: "1:479702387333:web:3f689952b5eafb4509b7ec",
+    measurementId: "G-1VCS4WRP7V"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
